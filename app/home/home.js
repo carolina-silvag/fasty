7b210e092fa3af0e99503d7350b85ab294a37e48
carolina silva <32287504+carolina-silvag@users.noreply.github.com>
2019-02-17 00:23:13 -0300

    Update home.js

--- app/home/home.js
+++ app/home/home.js
@@ -1,8 +1,8 @@
 function play(number) {
-	if(number == 1)
+    if(number == 1)
     	window.location.href = "../exercise/exercise.html"
     else if (number == 2)
     	window.location.href = "../exercise/exercise.html"
-   	else
-   		window.location.href = "../exercise/exercise.html"
+    else
+   	window.location.href = "../exercise/exercise.html"
 }
